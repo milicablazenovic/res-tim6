56989b9943f1370d521d35a6008541783b725de5
dajanam031 <dajanamisevici6@gmail.com>
2022-05-28 15:22:39 +0200

    konekcija i CRUD operacije sa bazom podataka

---
+++ databaseCRUD.py
@@ -0,0 +1,8 @@
+import cx_Oracle
+connection = cx_Oracle.connect("baza_res", "res", "localhost/xe")
+def readUsers():
+    cursor = connection.cursor()
+    cursor.execute("select * from brojilo")
+
+    for item in cursor:
+        print(item)
