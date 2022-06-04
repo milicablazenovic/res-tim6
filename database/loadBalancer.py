974eed9605e603a8ebee567a8b89030a46341438
Miljana <miljanabarjaktarovic9@gmail.com>
2022-06-05 01:56:53 +0200

    Inicijalna struktura komponente

---
+++ database/loadBalancer.py
@@ -0,0 +1,13 @@
+class LoadBalancer:
+
+    # TODO: Prima podatke od Writera i smesta ih u datoteku
+    def receiveData(self):
+        pass
+
+    # TODO: Prikuplja 10 vrednosti iz datoteke
+    def getData(self):
+        pass
+
+    # TODO: Prosledjuje prikupljene vrednosti prvom slobodnom Workeru
+    def forwardData(self):
+        pass
