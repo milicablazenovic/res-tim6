9b568d00f45b97277d242d7d3faff5979cb4c44a
Miljana <miljanabarjaktarovic9@gmail.com>
2022-06-07 17:45:28 +0200

    Preuzimanje i parsiranje podataka iz datoteke

--- loadbalancer_component/loadbalancer_class.py
+++ loadbalancer_component/loadbalancer_class.py
@@ -1,3 +1,5 @@
+from pkgutil import get_data
+from re import I, T
 from select import select
 import socket                                         
 import time
@@ -16,12 +18,12 @@ class LoadBalancer:
                 data = writersocket.recv(1024).decode()
 
                 # ukoliko je taj podatak 'end' -> prekidam konekciju i sa strane load balancera
-                if (data == "end"):
+                if (data == 'end'):
                     break
 
                 writersocket.send('Uspesno poslata poruka na server.'.encode())
 
-                # upisi ga u datoteku:
+                # upisi ga u datoteku:                
                 self.receive_data(data)
             except socket.error as e:
                 print('Greska: ' + str(e))
@@ -51,17 +53,68 @@ class LoadBalancer:
 
     def receive_data(self, data):        
         try:
-            buffer = open ('buffer.txt', 'a')
+            buffer = open('buffer.txt', 'a')
             buffer.write(data + '\n')
             buffer.close()
         except OSError:
             print('Neuspesno otvaranje fajla!')
             exit()
 
-    def get_data(self):
-        # TODO: Prikuplja 10 vrednosti iz datoteke
-        pass
+        # nakon sto se sacuva nesto u buffer.txt, pozivam funkciju koja ce da radi nad tim podatkom
+        self.get_data()
 
-    def forward_data(self):
-        # TODO: Prosledjuje prikupljene vrednosti prvom slobodnom Workeru
+    def parse_data(self, line_array):    
+        # npr. 16:44:56 06/07/22 - 29 : 5217           
+        dictionary = {}
+
+        for line in line_array:               
+            line_pieces = line.split(" ")
+            
+            id = int(line_pieces[3])
+            value = int(line_pieces[5])
+
+            dictionary[id] = value
+
+        # salje ih funkciji koja ce te vrednosti da prosledi prvom slobodnom workeru
+        self.forward_data(dictionary)
+
+    
+    def get_data(self):
+        line_array = []
+        try:
+            f = open("buffer.txt", "r")
+
+            count = 0
+            # prikuplja 10 vrednosti iz datoteke:
+            for line in f:
+                if (count < 10):
+                    line_array.append(line)
+                    count += 1
+                else:
+                    break
+
+            # prikupio 10 vrednosti:
+            if (count == 10):
+                # salje ih na parsiranje:
+                self.parse_data(line_array) 
+
+                # brise ih iz buffer.txt kako bi primio novih 10 vrednosti:
+                self.delete_data()
+
+            f.close()
+        except IOError:
+            print("Greska pri otvaranju fajla!")
+
+    def delete_data(self):
+        fin = open('buffer.txt', 'r')
+        data = fin.read().splitlines(True)
+
+        fout = open('buffer.txt', 'w')
+        fout.writelines(data[10:])
+
+    # TODO: prosledjuje prikupljene vrednosti prvom slobodnom Workeru
+    def forward_data(self, dictionary):
+        print("Vrednosti koje ce se slati prvom slobodnom workeru:")
+        for key, value in dictionary.items():
+            print(key, ' : ', value)
         pass
