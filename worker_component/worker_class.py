13695b9bf423ae2b3e3134b2d19b7dce8c435dc3
dajanam031 <dajanamisevici6@gmail.com>
2022-06-11 13:44:54 +0200

    Dodavanje foldera za Workera

---
+++ worker_component/worker_class.py
@@ -0,0 +1,16 @@
+
+class Worker:
+    def __init__(self):
+        pass
+
+    def start(self):
+        # TODO: uspostavljanje komunikacije sa Load balancerom
+        pass
+
+    def recieve_data(self):
+        # TODO: preuzimanje podataka od Load balancera
+        pass
+
+    def save_data(self):
+        # TODO: cuvanje podataka u bazu vodeci racuna da se ne upise u isti red 
+        pass
